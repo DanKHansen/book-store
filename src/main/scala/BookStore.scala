bca2898ce5c8dfbb87c057a947cfafad6aabcfd4
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-05-05 15:16:21 +0200

    refactorizing

--- src/main/scala/BookStore.scala
+++ src/main/scala/BookStore.scala
@@ -1,10 +1,23 @@
 object BookStore:
-   def total(l: List[Int]): Int =
+   private val discounts = Seq(800, 760, 720, 640, 600)
 
-      val m = l.groupBy(identity)
-      m.size match
-         case 2 => (m.map(_._2.size * 800).sum * 0.95).toInt
-         case 3 => (m.map(_._2.size * 800).sum * 0.90).toInt
-         case 4 => (m.map(_._2.size * 800).sum * 0.80).toInt
-         case 5 => (m.map(_._2.size * 800).sum * 0.75).toInt
-         case _ => m.map((*, books) => books.size * 800).sum
+   def total(books: Seq[Int]): Int =
+      val counts = books.groupBy(identity).view.mapValues(_.size).toMap
+      val bookCounts = counts.keys.toSeq.sorted.map(counts).toArray
+      def calc(counts: Seq[Int], memo: Map[Seq[Int], Int]): (Int, Map[Seq[Int], Int]) =
+         if counts.forall(_ == 0) then (0, memo)
+         else
+            memo.get(counts).map((_, memo)).getOrElse {
+               (1 until 1 << counts.size).foldLeft((Int.MaxValue, memo)) { case ((minP, m), mask) =>
+                  val subset = counts.indices.filter(i => (mask & (1 << i)) != 0)
+                  if subset.forall(counts(_) > 0) then
+                     val price = discounts(subset.size - 1) * subset.size
+                     val newCounts = counts.zipWithIndex.map { (c, i) => if subset.contains(i) then c - 1 else c }
+                     val (p, m2) = calc(newCounts, m)
+                     val newMin = price + p
+                     val newMemo = m2.updated(counts, Math.min(minP, newMin))
+                     (Math.min(minP, newMin), newMemo)
+                  else (minP, m)
+               }
+            }
+      calc(bookCounts.toSeq, Map.empty)._1
